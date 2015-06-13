26d590b7b13a7f2b448a579f84344e35e17b82e9
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-06-13 12:47:35 +0200

    Create makeSupportFileZip.sh

---
+++ miscScripts/makeSupportFileZip.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script generates the support file .zip archive
+
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+OUT_ZIP="$THIS_DIR/../ATAC_support_files.zip"
+
+zip -r "$OUT_ZIP" "$THIS_DIR/../bowtie2Index" "$THIS_DIR/../genomeSize" "$THIS_DIR/../inputData" "$THIS_DIR/../miscScripts" "$THIS_DIR/../README.md" "$THIS_DIR/../runATACPipeline.sh" "$THIS_DIR/../vPlotIndex"
