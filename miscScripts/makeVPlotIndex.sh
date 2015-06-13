dc16993ee4074554bc4af10205601aee601b3f7d
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-06-13 11:10:45 +0200

    update to include mm10

--- miscScripts/makeVPlotIndex.sh
+++ miscScripts/makeVPlotIndex.sh
@@ -16,8 +16,12 @@ makeVplotIndex(){
 model=$1
 trackName=$2
 saveDir=$3
+echo "Making V-Plot index for $1, $2 and saving to $3..."
 curl  -s "http://hgdownload.cse.ucsc.edu/goldenPath/${model}/database/${trackName}.txt.gz" | gunzip -c | awk -v OFS='\t' '{if ($3 == "+") {print $2, $4, $4+1, $3} else {print $2, $5, $5+1, $3}}'| sed 's/_[^\t]*\t/\t/' | sed '/chr[0-9]\|chrX\|chrY/!d' | sort -V | uniq > "${saveDir}/${trackName}_${model}vPlotIndex.bed"
+echo Done!
+echo
 }
 
 makeVplotIndex hg19 knownGene "${outdir}"
 makeVplotIndex mm9 knownGene "${outdir}"
+makeVplotIndex mm10 knownGene "${outdir}"
