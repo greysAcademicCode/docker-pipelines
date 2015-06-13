5a8bc7f618b5dae1059c2b3120da237fe742c83a
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-06-13 12:07:42 +0200

    update to generate output in parent directory

--- miscScripts/makeChromSizes.sh
+++ miscScripts/makeChromSizes.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# this script gets the chromosome size files for various models
+# this script generates the chromosome size files for various models
+
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 declare -a MODELS=("mm9" "mm10" "hg19" "hg38")
-OUT_DIR="./genomeSize"
+OUT_DIR="${THIS_DIR}/../genomeSize"
 
 mkdir -p "${OUT_DIR}"
 
@@ -13,4 +15,3 @@ for MODEL in "${MODELS[@]}"; do
   curl -s "http://hgdownload.cse.ucsc.edu/goldenPath/${MODEL}/bigZips/${MODEL}.chrom.sizes" | sed '/chr[0-9]\|chrM\|chrX\|chrY/!d' | sed '/_/d' > "${SAVE_FILE}"
   echo "Done!"
 done
-
