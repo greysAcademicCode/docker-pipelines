8a2aa67a3881d2c1a58997472770297b0374a9f1
Grey Christoforo <grey@christoforo.net>
2015-06-13 11:56:09 +0200

    add script to generate genome size files and build new ones for mm10 and hg38

---
+++ miscScripts/makeChromSizes.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# this script gets the chromosome size files for various models
+
+declare -a MODELS=("mm9" "mm10" "hg19" "hg38")
+OUT_DIR="./genomeSize"
+
+mkdir -p "${OUT_DIR}"
+
+for MODEL in "${MODELS[@]}"; do
+  SAVE_FILE="${OUT_DIR}/${MODEL}.genome"
+  echo "Genome size index for ${MODEL} and saving to ${SAVE_FILE}"...
+  curl -s "http://hgdownload.cse.ucsc.edu/goldenPath/${MODEL}/bigZips/${MODEL}.chrom.sizes" | sed '/chr[0-9]\|chrM\|chrX\|chrY/!d' | sed '/_random/d' > "${SAVE_FILE}"
+  echo "Done!"
+done
+
