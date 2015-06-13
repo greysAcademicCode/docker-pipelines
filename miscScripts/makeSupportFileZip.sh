e352a0aec53f029bd6ffe120941c1b660c13331a
Grey Christoforo <grey@christoforo.net>
2015-06-13 22:41:38 +0200

    re-do zip creation

--- miscScripts/makeSupportFileZip.sh
+++ miscScripts/makeSupportFileZip.sh
@@ -3,6 +3,6 @@
 # This script generates the support file .zip archive
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-OUT_ZIP="$THIS_DIR/../ATAC_support_files.zip"
-
-zip -v -r "$OUT_ZIP" "$THIS_DIR/../bowtie2Index" "$THIS_DIR/../genomeSize" "$THIS_DIR/../inputData" "$THIS_DIR/../miscScripts" "$THIS_DIR/../README.md" "$THIS_DIR/../runATACPipeline.sh" "$THIS_DIR/../vPlotIndex"
+OUT_ZIP="pipeline_support_files.zip"
+cd "$THIS_DIR/.."
+zip -v -r "$OUT_ZIP" "bowtie2Index" "genomeSize" "inputData" "miscScripts" "README.md" "runATACPipeline.sh" "vPlotIndex"
