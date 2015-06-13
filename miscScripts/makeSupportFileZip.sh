1ccad29654b39bfdc1c878b71850b08dbae37c47
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-06-13 12:50:16 +0200

    add verbosity to zip command

--- miscScripts/makeSupportFileZip.sh
+++ miscScripts/makeSupportFileZip.sh
@@ -5,4 +5,4 @@
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OUT_ZIP="$THIS_DIR/../ATAC_support_files.zip"
 
-zip -r "$OUT_ZIP" "$THIS_DIR/../bowtie2Index" "$THIS_DIR/../genomeSize" "$THIS_DIR/../inputData" "$THIS_DIR/../miscScripts" "$THIS_DIR/../README.md" "$THIS_DIR/../runATACPipeline.sh" "$THIS_DIR/../vPlotIndex"
+zip -v -r "$OUT_ZIP" "$THIS_DIR/../bowtie2Index" "$THIS_DIR/../genomeSize" "$THIS_DIR/../inputData" "$THIS_DIR/../miscScripts" "$THIS_DIR/../README.md" "$THIS_DIR/../runATACPipeline.sh" "$THIS_DIR/../vPlotIndex"
