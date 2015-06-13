31f145458bf3a5311b604b041d70c973172c9605
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-06-13 12:53:33 +0200

    avoid symlinks in pwd

--- miscScripts/makeSupportFileZip.sh
+++ miscScripts/makeSupportFileZip.sh
@@ -2,7 +2,7 @@
 
 # This script generates the support file .zip archive
 
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 OUT_ZIP="$THIS_DIR/../ATAC_support_files.zip"
 
 zip -v -r "$OUT_ZIP" "$THIS_DIR/../bowtie2Index" "$THIS_DIR/../genomeSize" "$THIS_DIR/../inputData" "$THIS_DIR/../miscScripts" "$THIS_DIR/../README.md" "$THIS_DIR/../runATACPipeline.sh" "$THIS_DIR/../vPlotIndex"
