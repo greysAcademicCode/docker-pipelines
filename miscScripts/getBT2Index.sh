c83ec56d0b11634dea5118622f39a85014771280
Grey Christoforo <grey@christoforo.net>
2017-04-18 09:58:21 +0100

    bt2 index fetching now needs python

--- miscScripts/getBT2Index.sh
+++ miscScripts/getBT2Index.sh
@@ -4,12 +4,15 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-URL_BASE='https://googledrive.com/host/0B6SEJBJh9kL3flNRQ2x3SWhCUU1RUjdNb0NBVmcxVkR0THF1VEhzSU9ZamxzOXA1VHhEOTg'
-
 EXPECTED="$THIS_DIR/../bowtie2Index/filesExpected.txt"
 
 cd "$THIS_DIR/../bowtie2Index"
 while read line; do
-  line==${line%$'\r'}
-  curl -L -O "$URL_BASE/${line}"
+  line=${line%$'\r'}
+  pair=($line) # file name and download ID
+  destination=${pair[0]}
+  fileid=${pair[1]}
+  echo "Downloading $destination"
+  python "$THIS_DIR/googleDriveGetFile.py" $fileid $destination
+  echo "Done."
 done <"$EXPECTED"
